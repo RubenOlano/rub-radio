748b057f9b2db0423fa23bbd25184df53887211b
Ruben Olano <rubendavid2711@gmail.com>
2024-09-30 00:00:48 -0500

    Update for node 18

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,27 +1,24 @@
-import { renderers } from "./renderers.mjs";
-import { c as createExports } from "./chunks/entrypoint_hb8HaOuJ.mjs";
-import { manifest } from "./manifest_C--IZi7N.mjs";
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_DhZ5pzqe.mjs';
+import { manifest } from './manifest_CBt43VEY.mjs';
 
-const _page0 = () => import("./pages/_image.astro.mjs");
-const _page1 = () => import("./pages/index.astro.mjs");
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
 
 const pageMap = new Map([
-  [
-    "node_modules/.pnpm/astro@4.15.9_rollup@4.22.5_typescript@5.6.2/node_modules/astro/dist/assets/endpoint/generic.js",
-    _page0,
-  ],
-  ["src/pages/index.astro", _page1],
+    ["node_modules/.pnpm/astro@4.15.9_rollup@4.22.5_typescript@5.6.2/node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
 ]);
 const serverIslandMap = new Map();
 const _manifest = Object.assign(manifest, {
-  pageMap,
-  serverIslandMap,
-  renderers,
-  middleware: () => import("./_noop-middleware.mjs"),
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-  middlewareSecret: "50d857b4-9aca-4bc2-b99b-4e4504094aed",
-  skewProtection: false,
+    "middlewareSecret": "f10c44f0-2db0-420b-858d-13bbe416dc05",
+    "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
